442001bf6db6f7ff29532063b7cb9d31a2fc78f7
hrsvrn <harshvardhanvatsa@gmail.com>
2024-08-05 00:56:48 +0530

    first commit

---
+++ client/src/Post.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {formatISO9075} from "date-fns"
+export default function Post({title, summary, cover, content ,createdAt,author}) {
+  return (
+    <div className="post">
+      <div className="image">
+        <img
+          src={'http://localhost:4000'+cover}
+          alt="Post cover"
+        />
+      </div>
+
+      <div className="texts">
+        <h2>{title}</h2>
+        <p className="info">
+          <a className="author">{author.username}</a>
+          <time>{formatISO9075(new Date(createdAt))}</time>
+        </p>
+        <p className="summary">
+          {summary}
+        </p>
+      </div>
+    </div>
+  );
+}
+
